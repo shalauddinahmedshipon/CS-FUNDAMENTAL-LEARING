a265c2d568b5096132e334d9ba4dea58447558a9
MD. SHIPON <sshimi910@gmail.com>
2025-10-10 01:10:34 +0600

    assignment1 done

---
+++ introduction to programming language/assginment-1/pb-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+
+    long long int A;
+     long long int B;
+    scanf("%lld %lld",&A,&B);
+     long long int mul = A*B;
+    printf("%lld",mul);
+    return 0;
+}
---
+++ introduction to programming language/module-3.5-practice/exam1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Hello, world! I am learning C programming language. ^_^
+// Programming is fun and challenging. /\/\/\
+// I want to give my 100% dedication to learn!    I will succeed one day.
+int main(){
+      char a=\ ;
+        printf("%c",a);
+
+    return 0;
+}
