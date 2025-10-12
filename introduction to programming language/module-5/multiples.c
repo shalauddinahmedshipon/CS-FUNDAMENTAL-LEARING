da85d160936e5fb7431064e9ce35124609617484
MD. SHIPON <sshimi910@gmail.com>
2025-10-12 21:13:07 +0600

    problem :  Multiples

---
+++ introduction to programming language/module-5/multiples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int min(){
+   int ,B;
+   scnf("%d %d",&,&b);
+   if(b % ==0 ){
+    printf("Multiples");
+   }
+   else if( %b==0){
+    printf("Multiples");
+   }
+   else{
+    printf("No Multiples");
+   }
+    return 0;
+}
