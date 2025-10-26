6b839b76ebedeb8343bad06ebd64c5286fe79227
MD. SHIPON <sshimi910@gmail.com>
2025-10-26 21:08:51 +0600

    midterm - countMe1 done

---
+++ introduction to programming language/midterm/Count_Me_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+   int n;
+   scanf("%d",&n);
+   int a[n];
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+   int count2=0;
+   int count3=0;
+
+   for(int i=0;i<n;i++){
+    if(a[i]%2==0){
+        count2++;
+    }
+    if(a[i]%3==0&& a[i]%2!=0){
+        count3++;
+    }
+   }
+
+
+   printf("%d %d",count2,count3);
+    return 0;
+}
