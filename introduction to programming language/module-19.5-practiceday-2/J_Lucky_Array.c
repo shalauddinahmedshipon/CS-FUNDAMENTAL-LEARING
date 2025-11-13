c0a8b45d6e0ad6fba31ee76a6372340869ef4341
MD. SHIPON <sshimi910@gmail.com>
2025-11-13 22:35:02 +0600

    J - Lucky Array

---
+++ introduction to programming language/module-19.5-practiceday-2/J_Lucky_Array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+   int n;
+   scanf("%d",&n);
+   int a[n];
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+   int min=100001;
+   for(int i=0;i<n;i++){
+    if(a[i]<min){
+        min=a[i];
+    }
+   }
+   int count =0;
+   for(int i=0;i<n;i++){
+    if(a[i]==min){
+        count++;
+    }
+
+    
+   }
+
+   if(count%2!=0){
+        printf("Lucky");
+    }else{
+        printf("Unlucky");
+    }
+    return 0;
+}
