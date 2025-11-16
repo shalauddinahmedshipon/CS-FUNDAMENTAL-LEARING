2af4d9eaa81f261c2c250c55dc1f1c08687deb20
MD. SHIPON <sshimi910@gmail.com>
2025-11-16 19:38:04 +0600

    run first c++ code

---
+++ C++ For DSA/module-1-basic-C++/input.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char b;
+    double d;
+   
+    cin>>a>>b>>d;
+    // int t=b;
+    cout<<a<<" "<<(int)b<<" "<<d;
+
+   
+    return 0;
+}
---
+++ C++ For DSA/module-1-basic-C++/test.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+int main()
+{
+    int a=100;
+    char b='B';
+    double d= 3.1416;
+
+
+    std::cout<<"Bismillahir Rahmanir Rahim"<<std::endl<<"Alhamdulillah"<<std::endl<<a<<" "<<b<<" "<<d;
+    return 0;
+}
