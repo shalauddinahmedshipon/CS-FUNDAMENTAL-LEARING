72688421274576cb879a3466fba2390982f1391d
MD. SHIPON <sshimi910@gmail.com>
2025-11-16 21:42:21 +0600

    model-1 done

---
+++ C++ For DSA/module-1-basic-C++/setprecision.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<iomanip>;
+using namespace std;
+int main()
+
+{
+    int a=100;
+    char b='B';
+    double d= 3.1416;
+
+    cout<<fixed<<setprecision(3)<<d;
+   
+    
+    return 0;
+}
