f3005695508c1dfa8fd3c35d462dda04ed3ef899
MD. SHIPON <sshimi910@gmail.com>
2025-11-18 18:35:04 +0600

    F reversing

---
+++ C++ For DSA/practice-day-1/F_Reversing.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int n ;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        int j=(n-1)-i;
+        if(j>i){
+        swap(a[i],a[j]);
+        }
+       
+    }
+
+     for(int i=0;i<n;i++){
+        cout<<a[i]<<' ';
+    }
+    return 0;
+}
