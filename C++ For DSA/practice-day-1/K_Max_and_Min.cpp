d10ee5c7c44257f33485749f3a3487535c5fc431
MD. SHIPON <sshimi910@gmail.com>
2025-11-18 18:38:53 +0600

    k max and min done

---
+++ C++ For DSA/practice-day-1/K_Max_and_Min.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int mini= min({a,b,c});
+    int maxi=max({a,b,c});
+    cout<<mini<<" "<<maxi;
+    return 0;
+}
