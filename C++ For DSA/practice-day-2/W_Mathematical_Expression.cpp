5e35677e9b5f2919e21959daf6c7d87ad6228a26
MD. SHIPON <sshimi910@gmail.com>
2025-11-21 05:10:45 +0600

    W - Mathematical Expression

---
+++ C++ For DSA/practice-day-2/W_Mathematical_Expression.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    char s;
+    cin>>s;
+    int b;
+    cin>>b;
+    char q;
+    cin>>q;
+    int c;
+    cin>>c;
+
+    if(s=='+'){
+      if(a+b==c){
+        cout<<"Yes"<<endl;
+      }else{    
+        cout<<a+b<<endl;
+      }
+    }
+    else if(s=='-'){
+      if(a-b==c){
+        cout<<"Yes"<<endl;
+      }else{    
+        cout<<a-b<<endl;
+      }
+    }
+    else if(s=='*'){
+      if(a*b==c){
+        cout<<"Yes"<<endl;
+      }else{    
+        cout<<a*b<<endl;
+      }
+    }
+    
+
+
+    return 0;
+}
