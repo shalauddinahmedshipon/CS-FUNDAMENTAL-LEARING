c3a3b24fd902c2a9c28130bb217de0d91afad9ca
MD. SHIPON <sshimi910@gmail.com>
2026-01-22 16:17:00 +0600

    Strong Password

---
+++ road to xpsc/xpsc-1/A_Strong_Password.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        int i;
+        for (i = 1; i <s.size(); i++)
+        {
+           if(s[i]==s[i-1]){
+            break;
+           }
+        }
+
+       if(s[i-1]!='a') s.insert(s.begin()+i,'a');
+       else s.insert(s.begin()+i,'b');
+
+      cout<<s<<endl;
+    }
+    
+    return 0;
+}
