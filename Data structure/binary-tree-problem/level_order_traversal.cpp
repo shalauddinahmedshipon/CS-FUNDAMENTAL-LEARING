02142974a8a14b90e5389ec18ddb9019b8b8221a
MD. SHIPON <sshimi910@gmail.com>
2026-02-06 16:06:01 +0600

    level order traversal - code360

---
+++ Data structure/binary-tree-problem/level_order_traversal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+   template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+        vector<int> v;
+        queue<BinaryTreeNode<int>*> q;
+        if(root) q.push(root);
+        while(!q.empty()){
+            BinaryTreeNode<int>* p=q.front();
+            q.pop();
+
+            v.push_back(p->val);
+
+            if(p->left) q.push(p->left);
+            if(p->right) q.push(p->right);
+        }
+
+        return v;
+}
