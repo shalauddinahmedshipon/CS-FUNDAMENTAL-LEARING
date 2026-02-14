73789d0add9f33d2b8762d2e071de0062884cdee
MD. SHIPON <sshimi910@gmail.com>
2026-02-14 16:24:07 +0600

    adjacancy list implementation done

---
+++ Algorithms/week-1-graphs/adj_list.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n,e;
+    cin>>n>>e;
+
+    vector<int> adj_list[n+1];
+  
+    while (e--)
+    {
+       int a,b;
+       cin>>a>>b;
+       adj_list[a].push_back(b);
+       adj_list[b].push_back(a);
+    }
+    
+
+
+     for (int i = 1; i <= n; i++){
+        for(int x:adj_list[i])
+           cout<<x<<" ";
+       cout<<endl;    
+     }
+        
+           
+                
+                
+
+    
+    return 0;
+}
