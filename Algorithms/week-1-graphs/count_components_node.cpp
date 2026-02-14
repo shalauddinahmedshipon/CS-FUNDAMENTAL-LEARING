7fdd93e8437d4675011af9d8eca07321f4e4765e
MD. SHIPON <sshimi910@gmail.com>
2026-02-14 23:47:43 +0600

    number of nodes in each component in ascending order.

---
+++ Algorithms/week-1-graphs/count_components_node.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> adj_list[100005];
+bool vis[100005];
+
+
+int res=0;
+
+void dfs(int src){
+res++;
+vis[src]=true;
+for(int child:adj_list[src]){
+  if(!vis[child]){
+    dfs(child);
+  }  
+}
+
+}
+
+
+int main(){
+    int n,e;
+    cin>>n>>e;
+   
+    
+
+    memset(vis,false,sizeof(vis));
+
+    while (e--)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+       
+    }
+     
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+       if(!vis[i]){
+         dfs(i);
+         v.push_back(res);
+       }
+       res=0;
+    }
+    
+    
+
+    sort(v.begin(),v.end());
+
+    for (int x: v)
+    {
+        cout<<x<<" ";
+    }
+    
+    
+    
+    return 0;
+}
