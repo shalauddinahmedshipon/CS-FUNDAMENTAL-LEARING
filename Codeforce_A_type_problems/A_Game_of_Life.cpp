779feff85b251d3d800800c537c5db363c57fc3a
MD. SHIPON <sshimi910@gmail.com>
2026-03-25 00:24:14 +0600

    A_Game_of_Life

---
+++ Codeforce_A_type_problems/A_Game_of_Life.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+       int n,m;
+       cin>>n>>m;
+       string s;
+       cin>>s;
+       vector<int> v;
+       for (int j = 0; j < min(n,m); j++)
+       {
+           for (int i = 0; i < n; i++)
+           {
+              if(i==0){
+                  if(s[i]=='0'&&s[i+1]=='1'){
+                    v.push_back(i);
+                  }
+              }
+              else if(i==n-1){
+                  if(s[i]=='0'&&s[i-1]=='1'){
+                    v.push_back(i);
+                  }
+              }
+             else{
+                if(s[i]=='0'&&(s[i+1]-'0'+s[i-1]-'0')==1){
+                    v.push_back(i);
+                }
+             }
+
+           }
+
+           for(auto x:v){
+            s[x]='1';
+           }
+           
+       }
+       
+       cout<<s<<endl;
+    }
+    
+    return 0;
+}
