d5916c262008aae6c804fa826fe36b252473baca
MD. SHIPON <sshimi910@gmail.com>
2026-05-09 20:23:23 +0600

    1791C - Prepend and Append

---
+++ xpsc/week-5/C_Prepend_and_Append.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int l=0,r=n-1,ans=n;
+        while(l<=r){
+            if(s[l]==s[r]){
+                break;
+            }else{
+                ans-=2;
+                l++;
+                r--;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
