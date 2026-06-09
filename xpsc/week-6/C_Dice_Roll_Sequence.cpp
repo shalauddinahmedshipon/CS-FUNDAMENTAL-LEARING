e4bddf4be49fb9695aab856cedb4f0dfd8bca4e0
MD. SHIPON <sshimi910@gmail.com>
2026-06-09 18:25:43 +0600

    2195C - Dice Roll Sequence

---
+++ xpsc/week-6/C_Dice_Roll_Sequence.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin>>v[i];
+        }
+
+        int cnt=0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if(v[i]+v[i-1]==7||v[i]==v[i-1]){
+               cnt++;
+               i++;
+            }
+        }
+        
+        cout<<cnt<<endl;
+    }
+    
+    return 0;
+}
