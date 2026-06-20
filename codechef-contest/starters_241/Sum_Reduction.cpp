f74856e4d7bf3bf169f0789ad1014ad201fe9790
MD. SHIPON <sshimi910@gmail.com>
2026-06-20 23:46:19 +0600

    sum_reduction cc-staters_241 -> c

--- codechef-contest/starters_241/Sum_Reduction.cpp
+++ codechef-contest/starters_241/Sum_Reduction.cpp
@@ -1,59 +1,24 @@
-// #include<bits/stdc++.h>;
-// using namespace std;
+#include<bits/stdc++.h>;
+using namespace std;
+#define ll long long
 
-// int main(){
-//     int t;
-//     cin>>t;
-//     while (t--)
-//     {
-//         int n;
-//         cin>>n;
-//         vector<int> v(n);
-//         map<int,int> mp;
-      
-//         for (int i = 0; i < n; i++)
-//         {
-//             cin>>v[i];
-//             mp[v[i]]++;
-          
-//         }
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        ll sum=0,oR=0; 
+        for(auto &val:v){
+                 cin>>val;
+                 sum+=val;
+                 oR|=val;
+        } 
 
-        
+        if(sum==oR) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
 
-//         if(n==1){
-//             cout<<"Yes"<<endl;
-//         }else{
-       
-
-
-//         for (int i = 0; i < 100; i++)
-//         {
-            
-//         for (int i = 0; i < n-1; i++)
-//         {
-//             for (int j = i+1; j < n; j++)
-//             {
-//                 if((v[i]&v[j])==0){
-//                     int sum=v[i]+v[j];
-//                     v.push_back(sum);
-//                     v.erase(v[i]);
-//                     v.erase(v[j]);
-//                 }
-//             }
-            
-          
-//         }
-//         }
-        
-        
-
-        
-
-
-//         }
-        
-
-//     }
-    
-//     return 0;
-// }
+    }
+    return 0;
+}
