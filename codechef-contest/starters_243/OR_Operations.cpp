c1a7fa8b14bfcb345966c87c83e0d7701054329c
MD. SHIPON <sshimi910@gmail.com>
2026-06-20 22:39:34 +0600

    OR Operations

--- codechef-contest/starters_243/OR_Operations.cpp
+++ codechef-contest/starters_243/OR_Operations.cpp
@@ -13,9 +13,21 @@ int main(){
             cin>>v[i];
         }
 
-        int test = 1|2;
-        cout<<test<<endl;
+        int  target= 0,cnt=0;
+        for(int x:v){
+            target=target|x;
+        }
+
         
+        for (int i = 0; i < n; i++)
+        {
+            if(v[i]!=target){
+                if(i+1<n) v[i+1]=v[i]|v[i+1];
+                cnt++;
+            }
+        }
+        
+        cout<<cnt<<endl;
     }
     return 0;
 }
