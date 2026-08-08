0831aecb361617035dc6056531f6555c321b2a1f
MD. SHIPON <sshimi910@gmail.com>
2026-08-08 11:21:38 +0600

    Fonta

---
+++ codechef-contest/starters_227/Fonta.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    string s; cin>>s;
+    if(s[2]=='n'&&s[3]=='t'&&s[4]=='a') cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
