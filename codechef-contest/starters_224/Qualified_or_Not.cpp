266f8d1574e853cfd49f5017b6b774ef6f61fcdc
MD. SHIPON <sshimi910@gmail.com>
2026-08-14 20:05:43 +0600

    Qualified or Not

---
+++ codechef-contest/starters_224/Qualified_or_Not.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    if((n>=2*x) and (n>=2*y)) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+   return 0;
+  
+}
